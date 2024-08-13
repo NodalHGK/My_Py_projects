5b1818ca5573904a61c7b7bdf38c40efbf4deb10
NodalHGK <141242266+NodalHGK@users.noreply.github.com>
2024-08-13 21:19:31 +0300

    Add files via upload

---
+++ password-generator/main.py
@@ -0,0 +1,42 @@
+import random
+import list
+
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy
+# password = ""
+# #nr_letters = 4
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(list.letters)
+
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(list.symbols)
+
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(list.numbers)
+    
+# print(password)
+    
+#Hard
+
+password_list = []
+#nr_letters = 4
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(list.letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(list.symbols)
+
+for char in range(1, nr_numbers + 1):
+   password_list += random.choice(list.numbers)
+    
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char   
+
+print(f"You password is: {password}") 
+    
